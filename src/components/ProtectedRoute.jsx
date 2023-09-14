a21940f3163ba53b0ee04d7d914426fdd38bc4b4
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2023-09-14 12:01:10 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
-
+//  implementing authentication checks and route protection, ensuring that certain routes are only accessible to authenticated users.
 const ProtectedRoute = ({ children }) => {
     const {user} = UserAuth()
 
